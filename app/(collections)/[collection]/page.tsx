70095df31d13714492bc88a85caec91f722435a0
Trevor <tdillonbond@gmail.com>
2026-04-02 10:01:37 -0700

    chore: remove incorrectly named collection directory

--- app/(collections)/[collection]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation"
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function CollectionRoot(props: any) {
-  const params = await props.params
-  redirect(`/${params.collection}/overview`)
-}
