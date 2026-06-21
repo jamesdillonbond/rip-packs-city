880a97cbbe0144ab38f9bf553c46448f54866949
Trevor <tdillonbond@gmail.com>
2026-06-21 08:59:09 -0700

    chore: hard-gate collection Fast Break/RTR routes + remove inert concierge +Cart button

---
+++ app/(collections)/[collection]/road-to-the-ring/layout.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "next/navigation"
+
+// Hidden for launch - Road to the Ring feature parked. Delete this file (or revert) to re-enable the route.
+export default async function HiddenRouteLayout(props: {
+  children: React.ReactNode
+  params: Promise<{ collection: string }>
+}) {
+  const { collection } = await props.params
+  redirect(`/${collection}/overview`)
+}
